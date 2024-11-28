a6266f829194541a0f84a0e2f4a3ed061b68b33a
MaxDmytruk0404 <rordm279@gmail.com>
2024-11-28 15:26:38 +0200

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,112 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from './database/database.service';
+
+@Injectable()
+export class AppService {
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  // Збереження інформації коли користувач зареєструвався
+  async save(dto: any) {
+    return this.databaseService.post.create({
+      data: {
+        data: dto,
+      },
+    });
+  }
+
+  // Перевірка при воході чи є цей користувач в базі диних
+  async checkEmail(email: string): Promise<boolean> {
+    const post = await this.databaseService.post.findFirst({
+      where: {
+        data: {
+          path: '$.email',
+          equals: email,
+        },
+      },
+    });
+
+    return post !== null;
+  }
+
+  // отримання інформації про користувача
+  async getUserInfo(email: string): Promise<any> {
+    const user = await this.databaseService.post.findFirst({
+      where: {
+        data: {
+          path: '$.email',
+          equals: email,
+        },
+      },
+    });
+
+
+    return user?.data ?? null;
+  }
+
+  // Додавання рейтингу в масив films
+  async addRaiting(data: any, email: string): Promise<any> {
+
+    const user = await this.databaseService.post.findFirst({
+      where: {
+        data: {
+          path: '$.email',
+          equals: email, 
+        },
+      },
+    });
+  
+    if (!user) {
+      throw new Error('User not found');
+    }
+  
+    return this.databaseService.post.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        data: data
+      }
+
+    });
+  }
+
+  // Отримання коментарів
+  async getComentaries(comentarId: string): Promise<any> {
+    const user = await this.databaseService.post.findFirst({
+      where: {
+        data: {
+          path: '$.comentarId',
+          equals: comentarId, 
+        },
+      },
+    });
+
+    return user?.data ?? null;
+  }
+
+  // Додавання нових коментарів до масиву
+  async sendComentar(data: any, filmId: string): Promise<any> {
+    const user = await this.databaseService.post.findFirst({
+      where: {
+        data: {
+          path: '$.comentarId',
+          equals: filmId, 
+        },
+      },
+    });
+  
+    if (!user) {
+      throw new Error('User not found');
+    }
+  
+    return this.databaseService.post.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        data: data
+      }
+
+    });
+  }
+}
