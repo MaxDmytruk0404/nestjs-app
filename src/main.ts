e2585597510260fb2233d2032f0846e98d620a84
MaxDmytruk0404 <rordm279@gmail.com>
2024-11-30 12:23:39 +0200

    new comit

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200', // дозволяє запити лише з цього джерела
+    origin: 'http://localhost:4200',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
